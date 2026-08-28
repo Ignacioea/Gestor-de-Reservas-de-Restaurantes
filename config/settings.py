21500262aa23e14238a27641cb0067f3a11da178
Ignacio <ignxcioxguilxr@gmail.com>
2026-08-28 13:13:29 -0400

    error.css is not working

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_r2t$(^b_3gr3(0u_h+$q0=o$&@pjpwbjede2-6pq7&tu9juul'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "reservations",
 ]
 
 MIDDLEWARE = [
